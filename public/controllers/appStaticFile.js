0b3d90c93e47c88f25d0fa10db889d1b2216e461
Alexander Charters <Charters@Alexanders-MacBook-Pro.local>
2015-07-09 19:22:57 -0400

    Fixed all progress bars fingers crossed

--- public/controllers/appStaticFile.js
+++ public/controllers/appStaticFile.js
@@ -6,11 +6,23 @@ leaderboardApp.controller('leaderboardCtrl', function($scope, $http){
 		$http.get('/api/getAll')
 		.then(function(res){
 			$scope.leaders = res.data;
-			var total = 0;
+			var admin_total = 0;
+			var yellow_total = 0;
+			var green_total = 0;
+			var blue_total = 0;
+			var red_total = 0;
+			var orange_total = 0;
 			for (var i = 0; i < $scope.leaders.length; i++){
+
 				total += $scope.leaders[i].amount_raised;
 			}
-			$scope.progress = (total/7000 * 100) + '%';
+			$scope.admin_progress = (admin_total/7000 * 100) + '%';
+			$scope.yellow_progress = (yellow_total/7000 * 100) + '%';
+			$scope.green_progress = (green_total/7000 * 100) + '%';
+			$scope.blue_progress = (blue_total/7000 * 100) + '%';
+			$scope.red_progress = (red_total/7000 * 100) + '%';
+			$scope.orange_progress = (orange_total/7000 * 100) + '%';
+
 		})
 	}
 });
