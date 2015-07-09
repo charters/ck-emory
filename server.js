6488fc9415daa115cea426080ad233bf11cf6b91
Alexander Charters <Charters@Alexanders-MacBook-Pro.local>
2015-07-09 19:06:11 -0400

    Fixing counselor updates

--- server.js
+++ server.js
@@ -48,41 +48,7 @@ app.get('/api/scrape', function(req, res){
 		for(i in array) {
 			var split_url = array[i].split(";");
 			var request_url = split_url[0];
-			request(request_url, function (error, response, html){
-				if (!error && response.statusCode == 200) {
-					var $ = cheerio.load(html);
-					var fundraiser_name = $('div.profile_wrapper strong').text();
-					var fundraiser_url = response.request.uri.href;
-					var raised_amount = 0;
-					var current = 0;
-					$('table.recent_donations_table td').each(function(i, elem) {
-						var text = $(this).text();
-						if (text.indexOf("donated") > -1){
-							var parts = text.split("donated $");
-							current = parseInt(parts[1]);
-						}
-						else if (text.indexOf("July") > -1){
-							var parts = text.split(" ");
-							var date = parts[2];
-							date = parseInt(date.substring(0, date.length - 1));
-							if (date >= 4){
-								raised_amount += current;
-							}
-
-						}
-						else if (text.indexOf("August") > -1) {
-							raised_amount += current;
-						}
-					});
-
-					
-					Counselor.findOneAndUpdate({ url: fundraiser_url }, {amount_raised: raised_amount}, function(err, counselor) {
-						if (err) throw err;
-
-						console.log('Updated counselor with name: ' + fundraiser_name);
-					});
-				}
-			});
+			updateRequest(request_url);
 		}
 	});
 });
@@ -132,6 +98,44 @@ function makeRequest(url, real_name, color){
 				}
 			});
 
+}
+
+function updateRequest(url){
+	request(url, function (error, response, html){
+				if (!error && response.statusCode == 200) {
+					var $ = cheerio.load(html);
+					var fundraiser_name = $('div.profile_wrapper strong').text();
+					var fundraiser_url = response.request.uri.href;
+					var raised_amount = 0;
+					var current = 0;
+					$('table.recent_donations_table td').each(function(i, elem) {
+						var text = $(this).text();
+						if (text.indexOf("donated") > -1){
+							var parts = text.split("donated $");
+							current = parseInt(parts[1]);
+						}
+						else if (text.indexOf("July") > -1){
+							var parts = text.split(" ");
+							var date = parts[2];
+							date = parseInt(date.substring(0, date.length - 1));
+							if (date >= 4){
+								raised_amount += current;
+							}
+
+						}
+						else if (text.indexOf("August") > -1) {
+							raised_amount += current;
+						}
+					});
+
+					
+					Counselor.findOneAndUpdate({ url: fundraiser_url }, {amount_raised: raised_amount}, function(err, counselor) {
+						if (err) throw err;
+
+						console.log('Updated counselor with name: ' + fundraiser_name);
+					});
+				}
+			});
 }
 
 app.listen(port, function(){
